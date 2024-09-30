95c332cc77561e18cb163cfa7696c46c14fc93a4
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-09-30 20:30:42 +1000

    chore: version bump to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ publishing {
 		register<MavenPublication>("release") {
 			groupId = "dev.frozenmilk.dairy"
 			artifactId = "Util"
-			version = "1.0.1"
+			version = "1.0.2"
 
 			afterEvaluate {
 				from(components["kotlin"])
